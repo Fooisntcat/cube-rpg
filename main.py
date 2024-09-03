d9e56fd547ba04f28f65d6bac534b4bb7e476b6b
Fooisntcat <Fooisntcat@users.noreply.github.com>
2024-09-03 22:36:33 +0800

    Added while True

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from sprites import *
 from config import *
 import sys
 
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
 class Game:
     def __init__(self):
