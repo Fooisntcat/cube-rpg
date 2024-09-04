4b8cbbe4280cc6ec3352344f5af5d3a5248b0790
stealth-112 <m-10516727@moe-dl.edu.my>
2024-09-04 11:13:16 +0800

    added player

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import pygame
 import sys
 import os
+import time
 
 WIDTH = 1920
 HEIGHT = 1080
+x = 300
+y = 300
 
 pygame.init()
 
 info = pygame.display.Info() # You have to call this before pygame.display.set_mode()
 screen_width,screen_height = info.current_w,info.current_h
-
-window_width,window_height = WIDTH-10,HEIGHT-500
 win = pygame.display.set_mode((screen_width-10, screen_height-50))
 
 pygame.display.update()
@@ -25,15 +26,40 @@ WHITE = (255, 255, 255)
 cube = WHITE
 
 running = True
+vel = 10
+
+w=40
+h=60
 
 while running:
 
     pygame.draw.rect(win, (255, 255, 255), (0, 750, 1550, 100))
+    pygame.display.update()
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     pygame.display.flip()
+
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and x > vel:
+         x -= vel
+    if keys[pygame.K_RIGHT] and x < screen_width - vel - 60:
+        x += vel
+    if keys[pygame.K_UP] and y > vel:
+        y -= vel
+    if keys[pygame.K_DOWN] and y < screen_height - vel - 160:
+        y += vel
+
+    win.fill((0, 0, 0),(0 , 0, screen_width, 750))
+    pygame.draw.rect(win,(180,180,180),(x , y, 50, 50)) 
+    pygame.display.update()
+    
+
+
+    
+pygame.quit
 
 class Game:
     def __init__(self):
