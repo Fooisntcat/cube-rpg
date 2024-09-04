1b608d78371f9096b1636cc079271ed43e85bf3f
stealth-112 <m-10516727@moe-dl.edu.my>
2024-09-04 08:12:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,10 +30,11 @@ class Game:
 
 BLOCK_SIZE = 20
 cube_cord = [[WIDTH//4, HEIGHT//4]]
+WHITE = (255, 255, 255)
 def draw_objects():
     win.fill((0, 0, 0))
     for cord in cube_cord:
-        pygame.draw.rect(win, WHITE, pygame.Rect(pos[0], pos[1], BLOCK_SIZE, BLOCK_SIZE))
+        pygame.draw.rect(win, WHITE, pygame.Rect(cord[0], cord[1], BLOCK_SIZE, BLOCK_SIZE))
 
     #def update(self):
     
